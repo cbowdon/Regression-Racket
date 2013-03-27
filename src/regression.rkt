ebb44e494260f6acce339b80aed70c397b6bb64c
Chris Bowdon <c.bowdon@gmail.com>
2013-03-27 22:49:25 +0000

    Beginning implementation.

--- src/regression.rkt
+++ src/regression.rkt
@@ -15,21 +15,30 @@
 ;(: batch-update (Matrix Array Array (Real -> Real) Real -> Array))
 ;(define (batch-update x y theta hypothesis alpha)
 ;  (let* ([prod (matrix* x (matrix-transpose theta))]
-;		 [diff (- y (hypothesis prod))]
+;		 [diff (matrix- y (hypothesis prod))]
 ;		 [a/m (/ alpha (array-size y))])
 ;	(matrix+ theta (* a/m (matrix* x diff)))))
 
 (: features (Matrix Real))
-(define features (build-matrix 10 (cast 1 Integer) (lambda (m n) (cast m Real))))
+(define features (build-matrix 10 1 (lambda (m n) (cast m Real))))
 (: targets (Matrix Real))
-(define targets (build-matrix 10 (cast 1 Integer) (lambda (m n) (* 2 (cast m Real)))))
+(define targets (build-matrix 10 1 (lambda (m n) (* 2 (cast m Real)))))
 
 (: params (Matrix Real))
-(define params (make-matrix (cast 2 Integer) (cast 1 Integer) (cast 0 Real)))
+(define params (make-matrix 1 2 0.0))
 
-;(batch-update features targets params linear 0.02)
-;features
-;targets
-;params
+(: insert-bias ((Matrix Real) -> (Matrix Real)))
+(define (insert-bias mat)
+  (let ([bias-col (make-matrix (matrix-num-rows mat) 1 1)])
+	(matrix-augment (list bias-col mat))))
 
-(matrix* features (matrix-transpose params))
+targets
+features
+params
+
+; TODO make into function
+(matrix-scale (matrix* (matrix-transpose (insert-bias features))
+					   (matrix- targets 
+								(matrix* (insert-bias features) 
+										 (matrix-transpose params))))
+			  (/ 0.02 (matrix-num-rows targets)))
