89637a9f247276f77fb3c69a9f83128b598a052c
Chris Bowdon <c.bowdon@gmail.com>
2013-03-29 10:46:33 +0000

    Implementation of stochastic descent in progress.

--- src/regression.rkt
+++ src/regression.rkt
@@ -24,21 +24,6 @@
   (let ([bias-col (make-matrix (matrix-num-rows mat) 1 1.0)])
 	(matrix-augment (list bias-col mat))))
 
-; Stochastic Gradient Descent
-(: stochastic-descent ((Float -> Float)
-					   (Matrix Float) 
-					   (Matrix Float) 
-					   (Matrix Float) -> (Matrix Float)))
-(define (stochastic-descent hypothesis features targets params)
-  params)
-
-(: stochastic-update ((Matrix Float) Float (Matrix Float) Float Integer -> (Matrix Float)))
-(define (stochastic-update features target params f_ij m)
-  (let* ([prod (matrix-dot features params)]
-		 [diff (- target (linear prod))])
-	(displayln (/ (* f_ij alpha diff) m))
-	params))
-
 
 ; Batch Gradient Descent
 (: batch-descent ((Float -> Float)
@@ -53,18 +38,62 @@
 		   [hyp (matrix-map hypothesis prod)])
 	  (matrix+ params
 			   (matrix-scale 
-				 (matrix-transpose (matrix* (matrix-transpose features) (matrix- targets hyp)))
+				 (matrix-transpose 
+				   (matrix* 
+					 (matrix-transpose features) 
+					 (matrix- targets hyp)))
 				 (/ alpha (matrix-num-rows targets))))))
 
-  (: iter ((Matrix Float) Integer -> (Matrix Float)))
-  (define (iter theta steps)
+  (: iter (Integer (Matrix Float) -> (Matrix Float)))
+  (define (iter steps theta)
 	(if (eq? 0 steps) 
 	  theta
-	  (iter (batch-update theta) (sub1 steps))))
+	  (iter (sub1 steps) (batch-update theta))))
 
-  (iter params step-limit))
+  (iter step-limit params))
 
-; Testing
+; Stochastic Gradient Descent
+; TODO does not work
+(: stochastic-descent ((Float -> Float)
+					   (Matrix Float) 
+					   (Matrix Float) 
+					   (Matrix Float) -> (Matrix Float)))
+(define (stochastic-descent hypothesis features targets params)
+
+  (: stochastic-update-param ((Matrix Float) Float (Matrix Float) Float Integer -> Float))
+  (define (stochastic-update-param x_i y_i params f_ij m)
+	(let* ([prod (matrix-dot x_i params)]
+		   [diff (- y_i (linear prod))])
+	  (/ (* f_ij alpha diff) m)))
+
+  (: stochastic-update ((Matrix Float) Float (Matrix Float) Integer -> (Matrix Float)))
+  (define (stochastic-update x_i y_i params m)
+	(matrix-map 
+	  (lambda (x_ij) (stochastic-update-param x_i y_i params x_ij m))
+	  x_i))
+
+  (: iter-row (Integer (Matrix Float) -> (Matrix Float)))
+  (define (iter-row i params)
+	(if (eq? (matrix-num-rows targets) i)
+	  params
+	  (iter-row
+		(add1 i)
+		(stochastic-update 
+		  (matrix-row features i) 
+		  (matrix-ref targets i 0)
+		  params 
+		  (matrix-num-rows targets)))))
+
+  (: iter (Integer (Matrix Float) -> (Matrix Float)))
+  (define (iter steps theta)
+	(if (eq? 0 steps) 
+	  theta
+	  (iter (sub1 steps) (iter-row 0 theta))))
+
+  (iter step-limit params))
+
+
+; Test data
 (: features (Matrix Float))
 (define features (insert-bias-col (matrix-transpose (matrix [[1. 2. 3. 4. 5. 6. 7. 8. 9. 10.]]))))
 
@@ -74,7 +103,6 @@
 (: params (Matrix Float))
 (define params (make-matrix 1 2 0.0))
 
+; Tests
 (batch-descent linear features targets params)
 (stochastic-descent linear features targets params)
-
-(stochastic-update (matrix-row features 0) (matrix-ref targets 0 0) params (matrix-ref features 0 0) (matrix-num-rows targets))
